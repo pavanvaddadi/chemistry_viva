ff203830b19e6cb106c2e8118ccae0df52ae6e66
SaiSrinivasS <63333924+SaiSrinivasS@users.noreply.github.com>
2020-04-11 10:20:33 +0530

    case corrected

--- Copyright.php
+++ Copyright.php
@@ -1,3 +1,3 @@
 	<div class="footer">
-			<p class="text-dark">Copyright &copy; <?php echo date("Y") ?> Pavan Kumar & Sai Srinivas. All Rights Reserved.</p>
+			<p class="text-dark">Copyright &copy; <?php echo date("Y") ?> Pavan Kumar & Sai Srinivas. All rights reserved.</p>
 	</div>
